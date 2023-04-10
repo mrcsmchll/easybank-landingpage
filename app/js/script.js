c0c8f5458ac9bb09249b61518e3e35a9d91b5b32
mrcsmchll <manchinelli.m@gmail.com>
2023-04-10 20:27:18 -0300

    hamburger menu open-close animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,6 @@
-console.log('hello dude@');
+const btnHamburger = document.querySelector("#btnHamburger");
+
+btnHamburger.addEventListener("click", _ => {
+    !btnHamburger.classList.contains("open") ? btnHamburger.classList.add("open") : btnHamburger.classList.remove("open");
+})
+
