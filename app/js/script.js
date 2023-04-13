8ac89e12dc07695b67acc86e0f740dee3659f46d
mrcsmchll <manchinelli.m@gmail.com>
2023-04-12 23:58:07 -0300

    ham-menu open/close overlay animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,17 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay")
 
-btnHamburger.addEventListener("click", _ => {
-    !btnHamburger.classList.contains("open") ? btnHamburger.classList.add("open") : btnHamburger.classList.remove("open");
+btnHamburger.addEventListener("click", _ => { 
+
+    if(!header.classList.contains("open")) {//when open ham-menu
+        header.classList.add("open");
+        overlay.classList.add("fade-in");
+        overlay.classList.remove("fade-out");
+    } else {//when close ham-menu
+        header.classList.remove("open");
+        overlay.classList.remove("fade-in");
+        overlay.classList.add("fade-out");
+    }
 })
 
