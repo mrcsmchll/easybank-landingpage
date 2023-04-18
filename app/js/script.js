7e4930c4fa1ef8020e9c8edb66b29ead439ab0b8
mrcsmchll <manchinelli.m@gmail.com>
2023-04-17 21:16:13 -0300

    prevent scrolling when ham-menu is open

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body")
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay")
@@ -6,12 +7,14 @@ const fadeElements = document.querySelectorAll(".has-fade")
 btnHamburger.addEventListener("click", _ => {
 
     if (!header.classList.contains("open")) {//when open ham-menu
+        body.classList.add("no-scroll");
         header.classList.add("open");
         fadeElements.forEach(el => {
             el.classList.add("fade-in");
             el.classList.remove("fade-out");
         })
     } else {//when close ham-menu
+        body.classList.remove("no-scroll");
         header.classList.remove("open");
         fadeElements.forEach(el => {
             el.classList.remove("fade-in");
